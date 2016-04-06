1a36209bcd0a41112e1b93fb36685387e12228d5
Douglas Casserleigh <casserleigh@gmail.com>
2016-04-06 14:21:35 -0600

    Editied test to be more specific and fixed resulting rspec failures
    [#116326419]

--- spec/features/editing_cars_spec.rb
+++ spec/features/editing_cars_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 feature "Editing Cars" do
   scenario "can edit a car" do
     car = FactoryGirl.create(:car)
-  visit edit_car_path(car)
+
+    visit edit_car_path(car)
+
 
 
   fill_in "Make", :with => "Chevy"
   fill_in "Model", :with => "Camaro"
   fill_in "Year", :with => "1968"
   fill_in "Price", :with => "2400"
-  click_button "Update"
+  click_button "Update Car"
 
   expect(page).to have_content('Chevy')
 end
